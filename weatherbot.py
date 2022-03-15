86be2629188e69717765641358fe27459fab035b
kinofgl <kinofgl@gmail.com>
2022-03-15 18:08:05 +0800

    优化文件名

--- weatherbot.py
+++ weatherbot.py
@@ -17,22 +17,21 @@ forcast = qWeather.get_weather_forecast()
 # 实例化Euic
 euic = EduicContent()
 # 取得每日内容
-sentence = euic.get_daily_content()[0]
+# sentence = euic.get_daily_content()[0]
 # 取得每日图片
 image = euic.get_daily_content()[1]
 # 编辑要发送的信息
-md_message = '### 北海天气  \n  ' \
+md_message = '### 北海实时天气  \n  ' \
              '##### {forcast}  \n  ' \
              '![picture]({image})  \n  ' \
-             '##### {sentence}  \n  '\
-    .format(forcast=forcast, image=image, sentence=sentence)
+    .format(forcast=forcast, image=image,)
 
 # 接收系统传入的webhook和secret
-huihui_webhook = sys.argv[1]
-huihui_secret = sys.argv[2]
+bot_webhook = sys.argv[1]
+bot_secret = sys.argv[2]
 
 # 实例化DingtalkChatbot
-huihui = DingtalkChatbot(huihui_webhook, secret=huihui_secret)
+bot = DingtalkChatbot(bot_webhook, secret=bot_secret)
 
 # 发送消息
-huihui.send_markdown(title="今天天气", text=md_message, is_at_all=False)
+bot.send_markdown(title="北海实时天气", text=md_message, is_at_all=False)
